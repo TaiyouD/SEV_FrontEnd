ff601b2c26e28a147347e407c264b1e9a42085eb
JuliaFlores21 <julia.flores@eagles.oc.edu>
2023-03-10 16:39:24 -0600

    fix(server): change baseurl

--- src/services/services.js
+++ src/services/services.js
@@ -5,7 +5,7 @@ import Router from "../router.js";
 
 var baseurl = "";
 if (process.env.NODE_ENV === "development") {
-  baseurl = "http://localhost/performance/";
+  baseurl = "http://localhost/performance-t4/";
 } else {
   baseurl = "/performance-t4/";
 }
