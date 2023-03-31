5fcde4d438004c428ff37e54f9834755980d2290
JuliaFlores21 <julia.flores@eagles.oc.edu>
2023-03-30 20:41:28 -0500

    fix(services): add getAll for role

--- src/services/roleServices.js
+++ src/services/roleServices.js
@@ -1,28 +1,25 @@
 import apiClient from "@/services/services.js";
 
-export default {
-  getAll() {
-    return apiClient.get("/roles");
+export default { 
+  getRoleForUser(userId) {
+    return apiClient.get(`/role/userPerf/${userId}`);
   },
-  getAllForUser(userId) {
-    return apiClient.get("/roles/user/" + userId);
+  getAll() {
+    return apiClient.get("/role");
   },
   get(id) {
-    return apiClient.get(`/roles/${id}`);
+    return apiClient.get(`/role/${id}`);
   },
   create(data) {
-    return apiClient.post("/roles", data);
+    return apiClient.post("/role", data);
   },
   update(id, data) {
-    return apiClient.put(`/roles/${id}`, data);
+    return apiClient.put(`/role/${id}`, data);
   },
   delete(id) {
-    return apiClient.delete(`/roles/${id}`);
+    return apiClient.delete(`/role/${id}`);
   },
-//   deleteAll() {
-//     return apiClient.delete(`/roles`);
-//   },
-  findByTitle(title) {
-    return apiClient.get(`/roles?title=${title}`);
-  },
+  deleteAll() {
+    return apiClient.delete(`/role`);
+  }
 };
