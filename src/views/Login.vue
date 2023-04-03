7343a8337944e6fd866835325a61da37b678132d
Dustin Ortiz <dustin.ortiz@eagles.oc.edu>
2023-04-03 03:01:21 -0500

    Updated Login Page Look.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,20 @@
 
 <template>
 <div>
-    <v-img height=667 width="100%" cover src="../assets/music-background.jpg">
-    <br><br><br><br><br><br><br><br><br><br><br><br>
+  <v-img height=667 width="100%" cover src="../assets/music-background.jpg">
+    <br><br><br><br><br><br><br><br><br>
     <SocialLogin/>
-    </v-img>
-  </div>
+    <br>
+    <div style="display: flex; justify-content: center;">
+      <v-card class="custom-card">
+        <div style="text-align: center;">
+          You can <a href="http://www.oc.edu/password" target="_blank">reset your password</a> if you have forgotten it.<br><br>
+          This is an Oklahoma Christian University system. Unauthorized access is prohibited. Usage is subject to security testing and monitoring. Misuse is subject to criminal prosecution. Users have no expectation of privacy except as otherwise provided by applicable privacy laws.
+        </div>
+      </v-card>
+    </div>
+  </v-img>
+</div>
 </template>
 
 <script>
@@ -17,3 +26,10 @@ export default {
   },
 };
 </script>
+
+<style>
+.custom-card {
+  width: 400px;
+  height: 190px; 
+}
+</style>
