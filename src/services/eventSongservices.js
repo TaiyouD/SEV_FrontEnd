109d2f0a749e0d9277a781ecf945ed70256392d8
TaiyoD <t.dail@eagles.oc.edu>
2023-04-06 17:12:02 -0500

    Changed eventSongs back to singular and not plural

--- src/services/eventSongservices.js
+++ src/services/eventSongservices.js
@@ -2,22 +2,22 @@ import apiClient from "@/services/services.js";
 
 export default {
   getAll() { 
-    return apiClient.get("/eventsongs");
+    return apiClient.get("/eventsong");
   },
   getAllForUser(userId) { //lemme know if need this so I can add on the backend
-    return apiClient.get("/eventsongs/user/" + userId);
+    return apiClient.get("/eventsong/user/" + userId);
   },
   get(id) {
-    return apiClient.get(`/eventsongs/${id}`);
+    return apiClient.get(`/eventsong/${id}`);
   },
   create(data) {
-    return apiClient.post("/eventsongs", data);
+    return apiClient.post("/eventsong", data);
   },
   update(id, data) {
-    return apiClient.put(`/eventsongs/${id}`, data);
+    return apiClient.put(`/eventsong/${id}`, data);
   },
   delete(id) {
-    return apiClient.delete(`/eventsongs/${id}`);
+    return apiClient.delete(`/eventsong/${id}`);
   },
 //   deleteAll() {
 //     return apiClient.delete(`/eventsong`);
