877d61f43fb27b0451345f8e059f501cb8a61d5a
cjackson027 <c.jackson@eagles.oc.edu>
2023-04-15 12:50:34 -0500

    feat(view): fixing routing error on addcritique

--- src/views/AddCritique.vue
+++ src/views/AddCritique.vue
@@ -4,9 +4,11 @@
       <v-img src="../assets/music-notes-bg1.jpg" max-height="100" />
       <v-container>
         <v-toolbar>
-          <v-btn icon to="/">
+          <router-link class="routerLink" :to="{ name: 'maintaineventsession', params: { eventId: event.id } }">
+          <v-btn icon>
             <v-icon>mdi-arrow-left</v-icon>
           </v-btn>
+          </router-link>
           <v-toolbar-title>Create Critique</v-toolbar-title>
         </v-toolbar>
         <br/>
@@ -239,6 +241,7 @@
   import CritiqueServices from "../services/critiqueServices";
   import RoleServices from "../services/roleServices";
   import EventSessionServices from "../services/eventSessionServices";
+  import EventServices from "../services/eventServices";
   import Utils from "@/config/utils.js";
   
   export default {
@@ -277,6 +280,7 @@
             }
         },
         eventSession:{},
+        event:{},
         message: "Fill out the form below to critique the performance. Once completed, click the 'Save' button.",
       };
     },
@@ -308,6 +312,18 @@
         })
         .catch((e) => {
           this.message = e.response.data.message;
+        });
+        this.retrieveThisEvent();
+    },
+    async retrieveThisEvent() {
+        await EventServices.get(this.eventSession.eventId)
+        .then((response) => {
+            this.event = response.data;
+            console.log('event');
+            console.log(this.event);
+        })
+        .catch((e) => {
+            this.message = e.response.data.message;
         });
     },
     async retrieveStudentRole() {
@@ -369,4 +385,8 @@
   width: 96.5%;
 }
 
+.routerLink{
+     text-decoration: none;
+ }
 </style>
+
